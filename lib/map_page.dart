508857e9f6c357eb2b0be8aa3bbc80e938dee0eb
liaamg1 <lgustafsson71@gmail.com>
2025-09-20 17:24:27 +0200

    Added simpel map

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  late GoogleMapController mapController;
+  static const LatLng karlskrona = LatLng(56.1612, 15.5869);
+
+
+  void _onMapCreated(GoogleMapController controller){
+    mapController=controller;
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Map view"));
+    return Scaffold(
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: const CameraPosition(
+          target: karlskrona,
+          zoom: 11.0,
+        ),
+      ),
+    );
   }
 }
