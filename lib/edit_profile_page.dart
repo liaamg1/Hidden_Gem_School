16f6f68a71d468bba9e2b2f0a285bc638d4ab354
liaamg1 <lgustafsson71@gmail.com>
2025-09-23 19:27:12 +0200

    added comment for reused code snippet

--- lib/edit_profile_page.dart
+++ lib/edit_profile_page.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class EditProfilePage extends StatelessWidget {
-
   EditProfilePage({super.key});
   final TextEditingController nameController = TextEditingController();
   final TextEditingController bioController = TextEditingController();
-  
+
   Future<void> _saveProfile(BuildContext context) async {
     final user = FirebaseAuth.instance.currentUser;
     if (user != null) {
@@ -15,13 +14,14 @@ class EditProfilePage extends StatelessWidget {
         'name': nameController.text.trim(),
         'bio': bioController.text.trim(),
       }, SetOptions(merge: true));
-      
+
       if (!context.mounted) {
         return;
       }
       Navigator.pop(context);
     }
   }
+
   @override
   Widget build(BuildContext context) {
     //Reused code from profile page
@@ -40,29 +40,29 @@ class EditProfilePage extends StatelessWidget {
           final data = snapshot.data!.data() as Map<String, dynamic>;
           bioController.text = data['bio'] ?? '';
           nameController.text = data['name'] ?? '';
-        return Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: nameController,
-              decoration: const InputDecoration(labelText: 'Name'),
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: nameController,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                ),
+                TextField(
+                  controller: bioController,
+                  decoration: const InputDecoration(labelText: 'Bio'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _saveProfile(context);
+                  },
+                  child: const Text('Save'),
+                ),
+              ],
             ),
-            TextField(
-              controller: bioController,
-              decoration: const InputDecoration(labelText: 'Bio'),
-            ),
-            ElevatedButton(
-              onPressed: ()  {
-                _saveProfile(context);
-              },
-              child: const Text('Save'),
-            ),
-          ],
-        ),
-      );
-        }
-    )
+          );
+        },
+      ),
     );
   }
 }
