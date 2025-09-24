8ff277331b494f166da94e2d31b92baf522a7c4a
liaamg1 <lgustafsson71@gmail.com>
2025-09-24 21:59:49 +0200

    Added simple homepage and dependencies

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,40 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Home page"));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(25),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 124, 181, 227),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                "This is your home page.\n"
+                "Enjoy exploring the app.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
