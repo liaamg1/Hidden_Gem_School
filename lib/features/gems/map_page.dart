adc0a8c1ee2068dc19186fc48e35e094e36f62b2
liaamg1 <lgustafsson71@gmail.com>
2025-10-01 23:18:33 +0200

    Added pins on map from firebase

--- lib/features/gems/map_page.dart
+++ lib/features/gems/map_page.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -8,10 +10,48 @@ class MapPage extends StatefulWidget {
   State<MapPage> createState() => _MapPageState();
 }
 
+Future<Set<Marker>> getMarkersFromFirebase() async {
+  final user = FirebaseAuth.instance.currentUser;
+  final data = await FirebaseFirestore.instance
+      .collection('users')
+      .doc(user!.uid)
+      .collection('posts')
+      .get();
+
+  final markers = data.docs.map((doc) {
+    final docData = doc.data();
+    final location = docData['location'];
+    return Marker(
+      markerId: MarkerId(doc.id),
+      position: LatLng(location.latitude, location.longitude),
+      infoWindow: InfoWindow(
+        title: docData['title'],
+        snippet: docData['description'],
+      ),
+    );
+  });
+  final markersSet = markers.toSet();
+  return markersSet;
+}
+
 class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
-  static const LatLng karlskrona = LatLng(56.1612, 15.5869);
+  Set<Marker> _markers = {};
 
+  Future<void> loadMarkers() async {
+    final loadedMarkers = await getMarkersFromFirebase();
+    setState(() {
+      _markers = loadedMarkers;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadMarkers();
+  }
+
+  static const LatLng karlskrona = LatLng(56.1612, 15.5869);
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
@@ -25,18 +65,7 @@ class _MapPageState extends State<MapPage> {
           target: karlskrona,
           zoom: 11.0,
         ),
-        markers: {
-          Marker(
-            markerId: MarkerId("Karlskrona"),
-            position: LatLng(56.1823, 15.5893),
-            infoWindow: InfoWindow(title: "BTH best swimming spot"),
-          ),
-          Marker(
-            markerId: MarkerId("Torko"),
-            position: LatLng(56.1509, 15.3979),
-            infoWindow: InfoWindow(title: "Ronneby great hidden swimming spot"),
-          ),
-        },
+        markers: _markers,
       ),
     );
   }
